4652c35d5f2e5cc38eeca3c62cc1eb6b64e127ce
Newton Ni <nwtnni@gmail.com>
2018-06-11 07:36:51 -0400

    Token enum

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,141 @@
+/// Represents all valid lexical tokens in the Tiger language.
 #[derive(Debug)]
 pub enum Token {
 
+    // Keywords
+
+    /// `type`
+    Type,
+
+    /// `var`
+    Var,
+
+    /// `function`
+    Function,
+
+    /// `break`
+    Break,
+
+    /// `of`
+    Of,
+
+    /// `end`
+    End,
+
+    /// `in`
+    In,
+
+    /// `nil`
+    Nil,
+
+    /// `let`
+    Let,
+
+    /// `do`
+    Do,
+
+    /// `to`
+    To,
+
+    /// `for`
+    For,
+
+    /// `while`
+    While,
+
+    /// `else`
+    Else,
+
+    /// `then`
+    Then,
+
+    /// `if`
+    If,
+
+    /// `Array`
+    Array,
+
+    // Operators
+
+    /// `:=`
+    Assign,
+
+    /// `|`
+    Or,
+
+    /// `&`
+    And,
+
+    /// `>=`
+    Ge,
+
+    /// `>`
+    Gt,
+
+    /// `<`
+    Lt,
+
+    /// `<>`
+    Neq,
+
+    /// `=`
+    Eq,
+
+    /// `/`
+    Div,
+
+    /// `*`
+    Mul,
+
+    /// `-`
+    Sub,
+
+    /// `+`
+    Add,
+
+    /// `.`
+    Dot,
+
+    // Miscellaneous
+
+    /// `[`
+    LBrace,
+
+    /// `]`
+    RBrace,
+
+    /// `{`
+    LBrack,
+
+    /// `}`
+    RBrack,
+
+    /// `(`
+    LParen,
+
+    /// `)`
+    RParen,
+
+    /// `/*`
+    LComment,
+
+    /// `*/`
+    RComment,
+    
+    /// `;`
+    Semicolon,
+
+    /// `:`
+    Colon,
+
+    /// `,`
+    Comma,
+
+    // Literals
+    
+    Int(i32),
+
+    Str(String),
+
+    Ident(String),
 }
