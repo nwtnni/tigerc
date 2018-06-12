2043907919fd256908bb27acd9716a85ef78a869
Newton Ni <nwtnni@gmail.com>
2018-06-11 23:51:12 -0400

    More comment test cases

--- tests/test_parse.rs
+++ tests/test_parse.rs
@@ -19,4 +19,10 @@ generate!(bad, "parse", "-p", "parsedsol", "parsed", |exp: String, act: String| 
 good!(test_basic_add_01, "basic_add_01");
 good!(test_basic_add_02, "basic_add_02");
 
+good!(test_comment_01, "comment_01");
+good!(test_comment_02, "comment_02");
+good!(test_comment_03, "comment_03");
+good!(test_comment_04, "comment_04");
+good!(test_comment_05, "comment_05");
+
 bad!(test_bad_var_type_01, "bad_var_type_01");
