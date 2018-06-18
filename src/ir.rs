86286fc8195500c81e94202b78c174ad1571cb31
Newton Ni <nwtnni@gmail.com>
2018-06-18 15:14:14 -0400

    Implement Display for IR nodes

--- src/ir.rs
+++ src/ir.rs
@@ -1,9 +1,11 @@
+use std::fmt;
+
 use sym::{store, Symbol};
 use uuid::Uuid;
 
 use span::Span;
 
-#[derive(Clone)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Temp {
     id: Uuid,    
     name: Symbol,
@@ -19,7 +21,7 @@ impl Temp {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Label {
     id: Uuid,    
     name: Symbol,
@@ -39,7 +41,7 @@ pub enum Exp {
     Const(i32),
     Name(Label),
     Temp(Temp),
-    Binop(Binop, Box<Exp>, Box<Exp>),
+    Binop(Box<Exp>, Binop, Box<Exp>),
     Mem(Box<Exp>),
     Call(Box<Exp>, Vec<Exp>),
     ESeq(Box<Stm>, Box<Exp>),
@@ -49,10 +51,10 @@ pub enum Stm {
     Move(Exp, Exp),
     Exp(Exp),
     Jump(Exp, Vec<Label>),
-    CJump(Relop, Exp, Exp, Label, Label),
+    CJump(Exp, Relop, Exp, Label, Label),
     Seq(Box<Stm>, Box<Stm>),
     Label(Label),
-    Source(Span),
+    Comment(String),
 }
 
 pub enum Binop {
@@ -79,4 +81,84 @@ pub enum Relop {
     Ule,
     Ugt,
     Uge,
+}
+
+impl fmt::Display for Temp {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(fmt, "{}_{}", self.name, self.id.simple())
+    }
+}
+
+impl fmt::Display for Label {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(fmt, "{}_{}", self.name, self.id.simple())
+    }
+}
+
+impl fmt::Display for Exp {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+        | Exp::Const(n)        => write!(fmt, "(CONST {})", n),
+        | Exp::Name(l)         => write!(fmt, "(NAME {})", l),
+        | Exp::Temp(t)         => write!(fmt, "(TEMP {})", t),
+        | Exp::Binop(l, op, r) => write!(fmt, "(BINOP {} {} {})", l, op, r),
+        | Exp::Mem(e)          => write!(fmt, "(MEM {})", e),
+        | Exp::ESeq(s, e)      => write!(fmt, "(ESEQ {} {})", s, e),
+        | Exp::Call(f, args)   => {
+            write!(fmt, "(CALL {}", f).unwrap();
+            for arg in args {
+                write!(fmt, " {}", arg).unwrap();
+            }
+            write!(fmt, ")")
+        },
+        }
+    }
+}
+
+impl fmt::Display for Stm {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+        | Stm::Move(d, s)            => write!(fmt, "(MOVE {} {})", d, s),
+        | Stm::Exp(e)                => write!(fmt, "(EXP {})", e),
+        | Stm::Jump(e, _)            => write!(fmt, "(LABEL {})", e),
+        | Stm::CJump(l, op, r, t, f) => write!(fmt, "(CJUMP {} {} {} {} {})", l, op, r, t, f),
+        | Stm::Seq(s1, s2)           => write!(fmt, "(SEQ {} {})", s1, s2),
+        | Stm::Label(l)              => write!(fmt, "(LABEL {})", l),
+        | Stm::Comment(c)            => write!(fmt, "(COMMENT {})", c),
+        }
+    }
+}
+
+impl fmt::Display for Binop {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+        | Binop::Plus    => write!(fmt, "ADD"),
+        | Binop::Minus   => write!(fmt, "SUB"),
+        | Binop::Mul     => write!(fmt, "MUL"),
+        | Binop::Div     => write!(fmt, "DIV"),
+        | Binop::And     => write!(fmt, "LAND"),
+        | Binop::Or      => write!(fmt, "LOR"),
+        | Binop::LShift  => write!(fmt, "LSHIFT"),
+        | Binop::RShift  => write!(fmt, "RSHIFT"),
+        | Binop::ARShift => write!(fmt, "ARSHIFT"),
+        | Binop::XOr     => write!(fmt, "XOR"),
+        }
+    }
+}
+
+impl fmt::Display for Relop {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+        | Relop::Eq  => write!(fmt, "EQ"),
+        | Relop::Ne  => write!(fmt, "NE"),
+        | Relop::Lt  => write!(fmt, "LT"),
+        | Relop::Gt  => write!(fmt, "GT"),
+        | Relop::Le  => write!(fmt, "LE"),
+        | Relop::Ge  => write!(fmt, "GE"),
+        | Relop::Ult => write!(fmt, "ULT"),
+        | Relop::Ule => write!(fmt, "ULE"),
+        | Relop::Ugt => write!(fmt, "UGT"),
+        | Relop::Uge => write!(fmt, "UGE"),
+        }
+    }
 }
--- src/translate/mod.rs
+++ src/translate/mod.rs
@@ -1,1 +1,2 @@
 mod frame;
+mod translate;
