5ec581ec57b689e0110a4b1df7cda8a98a58a57b
Newton Ni <nwtnni@gmail.com>
2018-06-26 16:08:00 -0400

    Implement Frame constructor

--- src/translate/context.rs
+++ src/translate/context.rs
@@ -36,23 +36,53 @@ impl From<Access> for ir::Exp {
 }
 
 pub struct Frame {
-    name: ir::Label,
+    label: ir::Label,
+    prologue: Vec<ir::Stm>,
+    epilogue: Vec<ir::Stm>,
     map: FnvHashMap<Symbol, Access>,
     offset: i32,
+    size: usize,
 }
 
 impl Frame {
-    pub fn new(name: ir::Label, args: Vec<(Symbol, bool)>) -> Self {
-        unimplemented!()
+    pub fn new(label: ir::Label, args: Vec<(Symbol, bool)>) -> Self {
+        let mut map = FnvHashMap::default();
+        let mut prologue = Vec::new();
+        let mut offset = 0;
+        let mut size = 0;
+
+        for (i, (name, escape)) in args.iter().enumerate() {
+            let from = Frame::get_argument(i);
+            let to = if *escape {
+                offset -= 1;
+                size += 1;
+                Access::Frame(offset)
+            } else {
+                Access::Reg(ir::Temp::with_name("ARG")) 
+            };
+
+            prologue.push(ir::Stm::Move(from, to.into()));
+            map.insert(*name, to);
+        }
+
+        Frame {
+            label,
+            prologue,
+            epilogue: Vec::new(),
+            map,
+            offset,
+            size,
+        }
     }
 
-    pub fn name(&self) -> ir::Label {
-        self.name
+    pub fn label(&self) -> ir::Label {
+        self.label
     }
 
     pub fn allocate(&mut self, name: Symbol, escape: bool) -> ir::Exp {
         let access = if escape {
             self.offset -= 1;
+            self.size += 1;
             Access::Frame(self.offset)
         } else {
             Access::Reg(
@@ -66,6 +96,34 @@ impl Frame {
 
     pub fn get(&self, name: Symbol) -> Option<ir::Exp> {
         self.map.get(&name).map(|&access| access.into())
+    }
+
+    fn get_argument(i: usize) -> ir::Exp {
+        if i < 6 {
+            ir::Exp::Temp(
+                ir::Temp::with_reg(
+                    Reg::get_argument(i)
+                )
+            )
+        } else {
+            let fp = ir::Exp::Temp(
+                ir::Temp::with_reg(Reg::RBP)
+            );
+
+            let offset = ir::Exp::Const(
+                i as i32 - 6
+            );
+
+            ir::Exp::Mem(
+                Box::new(
+                    ir::Exp::Binop(
+                        Box::new(fp),
+                        ir::Binop::Add,
+                        Box::new(offset),
+                    )
+                )
+            )
+        }
     }
 }
 
