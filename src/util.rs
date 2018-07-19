5760fe02d5f903cb3d1bf98ebc54eef2206b593f
Newton Ni <nwtnni@gmail.com>
2018-07-19 16:12:50 -0400

    Void type for utility

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,14 @@
+use std::fmt;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Void {}
+
+impl fmt::Display for Void {
+    fn fmt(&self, _: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        Ok(())
+    }
+}
+
 #[macro_use]
 macro_rules! hashmap {
     ( $( $key:expr => $value:expr ),* ) => {
